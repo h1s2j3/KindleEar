294030fd80f45f457267e94f54f9bde53e7768ef
h1s2j3 <39670521+h1s2j3@users.noreply.github.com>
2018-05-27 20:19:28 +0800

    更改账号

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """Configures for KindleEar, the First two variable is must to modify.
 KindleEar配置文件，请务必修改开始两个配置（如果使用uploader，则uploader自动帮你修改）
 """
 
-SRC_EMAIL = "akindleear@gmail.com"  #Your gmail account for sending mail to Kindle
-DOMAIN = "http://kindleear.appspot.com/" #Your domain of app
+SRC_EMAIL = "h1s2j3@gmail.com"  #Your gmail account for sending mail to Kindle
+DOMAIN = "http://yuanmiaoyueshi.appspot.com/" #Your domain of app
 
 TIMEZONE = 8  #Default timezone, you can modify it in webpage after deployed
 
